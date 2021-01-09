90e1bce8a8f2e38ca844001003af1fcfdd38e726
JIeCoRyI3 <jiecoryi3@gmail.com>
2021-01-09 16:48:04 +0200

    Editing README

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
 
     </div>
   );
---
+++ src/components/Table/Table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Table.module.css';
+
+class Table extends React.Component {
+    render() {
+        return (
+            <div className={styles.Table}>
+
+            </div>
+        )
+    }
+}
